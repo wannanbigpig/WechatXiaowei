228cdcfb9356c924e08d084da1cad4ebd448f5c7
烟雨弥漫了江南 <723164417@qq.com>
2018-09-11 15:00:02 +0800

    Update WechatRoutes.php

--- V1/Config/WechatRoutes.php
+++ V1/Config/WechatRoutes.php
@@ -11,9 +11,9 @@
         $api->post('applystatus', 'WechatController@enquiryOfApplyStatus');    // 入驻状态查询
         $api->post('tenantconfig', 'WechatController@tenantConfig');    // 小微商户关注功能配置API
         $api->post('addsubdevconfig', 'WechatController@addSubDevConfig');    // 小微商户开发配置新增支付目录API
-        $api->post('bindappidconfig', 'WechatController@bindAppIdConfig');    // 小微商户开发配置新增支付目录API
+        $api->post('bindappidconfig', 'WechatController@bindAppIdConfig');    //
         $api->post('inquireconfig', 'WechatController@inquireConfig');    // 查询配置
         $api->post('modifyarchives', 'WechatController@modifyArchives');    // 修改银行卡号
-        $api->post('withdrawalstate', 'WechatController@withdrawalState');    // 修改银行卡号
-        $api->post('reautowithdrawbydate', 'WechatController@reAutoWithdrawByDate');    // 修改银行卡号
+        $api->post('withdrawalstate', 'WechatController@withdrawalState');    // 
+        $api->post('reautowithdrawbydate', 'WechatController@reAutoWithdrawByDate');    //
     });
